ee035b1c1f67332352f536e35833098e402d8788
A Glazer <avigayilglazer@gmail.com>
2019-01-17 18:48:42 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r (array)
+  array.select {|word| word.start_with?("r")}
+  if word.size > 1
+    "true"
+  else
+    "false"
+
+binding.pry
+  end
+
+end
