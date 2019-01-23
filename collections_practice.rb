8fcb5875226c0d868a0f52b2d363410a05fac690
A Glazer <avigayilglazer@gmail.com>
2019-01-23 17:51:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,14 +2,16 @@ require 'pry'
 
 def begins_with_r (array)
   array.all? {|word| word.start_with?("r")}
-
 end
 
 def contain_a (array)
-  array.collect |word|
+  array.collect do |word|
     if word.include?("a")
       word
     end
+  end.compact
+end
 
-  end
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
 end
