a29a547b8e0347ccf0a598c3e7a7b8c02b108cd7
A Glazer <avigayilglazer@gmail.com>
2019-01-24 20:30:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,3 +15,18 @@ end
 def first_wa(array)
   array.find {|word| word.to_s.start_with?("wa")}
 end
+
+def remove_non_strings(array)
+  array.delete_if {|word| !(word.is_a? String)}
+end
+
+def count_elements(hashes)
+new_array = []
+  hashes.each do |phrase|
+    phrase.each do |key, value|
+      value.select {|word| word == value }
+
+    end
+  end
+new_array
+end
